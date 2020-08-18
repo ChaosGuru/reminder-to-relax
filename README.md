59d649c4a0f58a64cb8e65da84203e23774e2419
ChaosGuru <chaos.feature@gmail.com>
2020-08-18 14:18:57 +0300

    some minor edits

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ReminderToRelax
-This remainder will remaind you to relax
+This program display text on full screen with transparent background.
+To shedule it you need use windows task sheduler.
 
 How to use?:
 1) Install requirements
@@ -11,4 +12,3 @@ pip install -r requirements.txt
 pyinstaller --noconsole main.py
 ```
 3) Add dist/main/main.py to task shedule
-...to be continue
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from tkinter import *
-from PIL import Image, ImageTk
 import time
 
+from tkinter import *
+from PIL import Image, ImageTk
+
+
 def timer():
-    tmr = 10 - int(time.perf_counter())
+    tmr = 60 - int(time.perf_counter())
 
     if tmr < 0:
         app.destroy()
 
     label.config(text='RELAX, let your eyes rest\n%s' % tmr)
     label.after(1000, timer)
+
 
 # window
 app = Tk()
@@ -21,11 +24,13 @@ app.wm_attributes("-disabled", True)
 app.state('zoomed')
 
 # label
-label = Label(app, font=('Segoe UI','100'), fg='#74B62E', bg='black')
+label = Label(app, font=('Segoe UI','100', 'bold'), fg='#74B62E', bg='black')
 label.pack()
 timer()
 
-# test with image
+# run
+app.mainloop()
+
 # It is possible to make photo with transperent background
 # just make sure pixels you want to be transperent have the same rgb or hex what you set
 # if there are annoying leftovers - recheck their rgb or hex
@@ -34,10 +39,3 @@ timer()
 # lebel_img1 = Label(app, image=render1, borderwidth=0)
 # lebel_img1.image = render1
 # lebel_img1.pack()
-
-app.mainloop()
-
-# room for modification
-# 1) add setting from json, for example displayed text, color
-# 2) differnt mods - annoying and not so much
-# 3) add motivation phrases
